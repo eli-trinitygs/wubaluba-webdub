02ef0bccf11283a92420773f5193799394021e7b
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-04-08 00:28:40 -0500

    definiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { AboutComponent } from './components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
